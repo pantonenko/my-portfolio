a96dba45e365c7558b64d8dde8012136360978ab
pantonenko <pantonenko@ufl.edu>
2025-09-09 00:20:00 -0400

    uploaded headshot & resume

--- src/components/Coursework.js
+++ src/components/Coursework.js
@@ -18,9 +18,11 @@ function Coursework() {
 
         <h3 className="text-xl font-semibold mb-3">Other Distinctions</h3>
         <ul className="list-disc pl-6 text-gray-700 space-y-2">
-          <li>Awards: Dean’s List (2024–2025)</li>
+          <li>Awards: </li>
+          <li>Dean’s List (2024–2025)</li>
           <li>NCWIT Aspirations in Computing Award (2024)</li>
-          <li>Clubs: CS Kickstart</li>
+          <li>Clubs: </li>
+          <li>CS Kickstart</li>
           <li>Society of Women Engineers (SWE)</li>
           <li>Women in Computer Science & Engineering (WiCSE)</li>
         </ul>
